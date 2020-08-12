962c285ce9e606aa43c4d781df5b850fba43353d
Hannah Angle <hannahcangle@gmail.com>
2020-08-12 05:58:47 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,7 +2,15 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative 'fancy_dance.rb'
 class Kid 
-extend FancyDance::ClassMethods
+  extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
 
 end 
